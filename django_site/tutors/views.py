37c7f858866088af442e7aedecc13d99b76e4946
jason-cmk <jas.kang@gmail.com>
2015-07-04 23:29:26 +1200

    Designed related tutors

--- django_site/tutors/views.py
+++ django_site/tutors/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 import datetime
 import math
 
-from .models import Tutor, Subject
+from .models import Tutor, Subject, TutorSubject
 
 
 def index(request):
@@ -12,8 +12,12 @@ def index(request):
     context = {'tutors_list': tutors_list, 'subjects_list': subjects_list}
     return render(request, 'tutors/PTG_Tutors.html', context)
 
+
 def profile(request, tutor_id):
+    # number of tutors
     number_of_tutors = Tutor.objects.count()
+
+    # get role(s)
     tutor = get_object_or_404(Tutor, pk=tutor_id)
     tutor_roles = []
     for role in tutor.tutorrole_set.all():
@@ -21,7 +25,24 @@ def profile(request, tutor_id):
     tutor_roles = ', '.join(tutor_roles)
     if tutor_roles:
         tutor_roles = tutor_roles[0] + tutor_roles.lower()[1:]
+
+    # calculate age
     age = datetime.datetime.now().date() - tutor.dob
     age = math.floor(age.days / 365)
 
-    return render(request, 'tutors/PTG_Profile.html', {'tutor': tutor, 'tutor_roles': tutor_roles, 'age': age, 'number_of_tutors': number_of_tutors})
+    # get related tutors
+    list_of_tutors = Tutor.objects.all().order_by('?')
+    related = []
+    for subject in tutor.tutorsubject_set.all():
+        for peer in list_of_tutors:
+            for peer_subject in peer.tutorsubject_set.all():
+                if subject.subject == peer_subject.subject and peer not in related and peer != tutor:
+                    related += [peer]
+
+    return render(request, 'tutors/PTG_Profile.html',
+                  {'tutor': tutor,
+                   'tutor_roles': tutor_roles,
+                   'age': age,
+                   'number_of_tutors': number_of_tutors,
+                   'related': related
+                   })
